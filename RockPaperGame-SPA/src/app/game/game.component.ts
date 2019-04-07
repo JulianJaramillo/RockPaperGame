f81d3c979c66318664cf7301bf15300dd0f577cd
JULIAN JARAMILLO PEREZ <jjarape@contratista.epm.co>
2019-04-06 19:09:28 -0500

    Cambios Logica Juego

--- RockPaperGame-SPA/src/app/game/game.component.ts
+++ RockPaperGame-SPA/src/app/game/game.component.ts
@@ -6,6 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  scores = [0 , 0];
+  weapons = [
+    'rock',
+    'paper',
+    'scissors'
+  ]
+  playerSelected = -1;
+  isResultShow = false;
+  
+  // theResult -  0 winner
+  //              1 lose
+  //              2 tie
+  theResult = 0 
+  enemySelected  = -1;
+
+ pick( weapon: number): void {
+   this.playerSelected = weapon;
+ }
+
+ pickEnemy( weapon: number): void {
+  this.enemySelected = weapon;
+  this.checkResult();
+  this.isResultShow = true;
+ }
+
+ reset(): void {
+  this.scores = [0,0];
+ }
+ checkResult(): void {
+   const playerPick = this.playerSelected;
+   const enemyPick = this.enemySelected;
+  
+   if( playerPick == enemyPick){
+    this.theResult = 2;
+    }
+
+    //You Win
+    else if ( (playerPick - enemyPick + 3)% 3 == 1)    {
+      this.theResult = 0;
+      this.scores[0] = this.scores[0]+1;
+      if(this.scores[0] == 3){
+        this.scores = [0,0];
+      }
+    }
+    //You Lose
+    else{
+      this.theResult = 1;
+      this.scores[1] = this.scores[1]+1;
+      if(this.scores[1] == 3){
+        this.scores = [0,0];
+      }
+    }
+ }
 
   constructor() { }
 
